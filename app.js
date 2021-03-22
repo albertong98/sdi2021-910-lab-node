009299f49ff4d60593082348e3b819c99c66abf5
albertong98 <albertng1998@gmail.com>
2021-03-22 10:54:03 +0100

    SDI-2021-910-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 let express = require('express');
 let app = express();
 
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('port',8081);
+
+app.use(express.static('public'));
 
 require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app);
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,15 +1,20 @@
 module.exports = (app) => {
 
-    app.get('/canciones', (req,res) => {
-        let s;
-        req.query.nombre != null ? s = 'Nombre: ' + req.query.nombre + '<br>' : "";
-        typeof(req.query.autor) != "undefined" ? s+='Autor: '+req.query.autor : "";
-        res.send(s);
-    });
+    app.get('/canciones', (req,res) => res.send(getCancion(req)));
 
     app.get('/suma', (req, res) => res.send(String(parseInt(req.query.num1) + parseInt(req.query.num2))));
 
     app.get('/canciones/:id', (req, res) => res.send( 'id: ' + req.params.id));
 
     app.get('/canciones/:genero/:id', (req, res) => res.send('id: ' + req.params.id + '<br>' + 'Género: ' + req.params.genero));
+
+    app.post('/cancion',(req,res) => res.send(postCancion(req)));
 };
+
+let getCancion = req => {
+    let res = req.query.nombre != null ? 'Nombre: ' + req.query.nombre + '<br>' : "";
+    typeof(req.query.autor) != "undefined" ? res+='Autor: '+req.query.autor : "";
+    return res;
+}
+
+let postCancion = req => 'Canción agregada '+req.body.nombre+'<br>'+'Genero: '+req.body.genero+'<br>'+'precio: '+req.body.precio
