ae2cd55deb5043ecfbbe81c8df795c35fc9d1915
albertong98 <albertng1998@gmail.com>
2021-04-05 09:15:11 +0200

    “SDI-2021-910-8.2-Arquitectura para el acceso a datos.”

--- app.js
+++ app.js
@@ -11,11 +11,15 @@ let mongo = require('mongodb');
 let dburi = 'mongodb://admin:sdi@tiendamusica-shard-00-00.vadns.mongodb.net:27017,tiendamusica-shard-00-01.vadns.mongodb.net:27017,tiendamusica-shard-00-02.vadns.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mt8n44-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 app.set('db',dburi);
+
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 app.set('port',8081);
 
 app.use(express.static('public'));
 
-require("./routes/rusuarios.js")(app,swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app,swig,mongo);
+require("./routes/rusuarios.js")(app,swig,gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app,swig,gestorBD);
 
 app.listen(app.get('port'), () => console.log("server activo"));
--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -1,1 +1,1 @@
-module.exports = (app,swig) => app.get('/usuarios', (req, res) => res.send("ver usuarios"));
+module.exports = (app,swig,gestorBD) => app.get('/usuarios', (req, res) => res.send("ver usuarios"));
