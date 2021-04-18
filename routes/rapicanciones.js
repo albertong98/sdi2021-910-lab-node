e2d28bbea21834b438ccad91a7f8260eebc67fa9
albertong98 <albertng1998@gmail.com>
2021-04-19 00:21:58 +0200

    SDI-2021-910-10.2-SW API REST Autenticación.

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -8,6 +8,8 @@ module.exports = (app,gestorBD) => {
     app.post('/api/cancion',(req,res) => agregarCancion(req,res,gestorBD));
 
     app.put('/api/cancion/:id',(req,res) => modificarCancion(req,res,gestorBD));
+
+    app.post('/api/autenticar/',(req,res) => autenticarUsuario(req,res,gestorBD));
 }
 
 let obtenerCanciones = (res,gestorBD) => {
@@ -104,5 +106,27 @@ let modificarCancion = (req,res,gestorBD) => {
                 _id : req.params.id
             })
         }
+    });
+}
+
+let autenticarUsuario = (req,res,gestorBD) => {
+    let seguro = app.get('crypto').createHmac('sha356',app.get('clave')).update(req.body.password).digest('hex');
+
+    let criterio = { email: req.body.email, password: seguro };
+
+    gestorBD.obtenerUsuarios(criterio, (usuarios) => {
+       if(usuarios == null || usuarios.length == 0){
+           res.status(401);
+           res.json({ autenticado: false });
+       }else{
+           let token = app.get('jwt').sign(
+               {usuario: criterio.email , tiempo: Date.now()/1000},
+               "secreto");
+           res.status(200);
+           res.json({
+               autenticado: true,
+               token : token
+           });
+       }
     });
 }
